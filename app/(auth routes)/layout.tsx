8bb4656d231d523554779e523d3cda28887329d4
FinJex <maks.vasylovych12@gmail.com>
2026-06-18 12:27:17 +0200

    hw

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,5 +1,22 @@
-const q = () => {
+'use client';
 
+import { useEffect, useState, startTransition } from 'react';
+import { useRouter } from 'next/navigation';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function PublicLayout({ children }: Props) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+    startTransition(() => {
+      setLoading(false);
+    });
+  }, [router]);
+
+  return <>{loading ? <div>Loading...</div> : children}</>;
 }
-
-export default q;
